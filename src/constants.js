adf4a1771ca5df5d2d180d2e36319e6b8cb9c217
unknown <yasutoko815@ever.ocn.ne.jp>
2022-05-24 04:58:39 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// CONTRACT_ADDRESSに、コントラクトアドレスを保存します。
+const CONTRACT_ADDRESS = "0x1Ad5a8aC62200A39aD6415a18BA45344506Bb860";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
