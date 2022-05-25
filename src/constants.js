d2d38e0c6f02fa1ba1ca16d9eb8699102d0c2d5a
unknown <yasutoko815@ever.ocn.ne.jp>
2022-05-26 07:33:57 +0900

    upload to github

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // CONTRACT_ADDRESSに、コントラクトアドレスを保存します。
-const CONTRACT_ADDRESS = "0x1Ad5a8aC62200A39aD6415a18BA45344506Bb860";
+const CONTRACT_ADDRESS = "0x0fe5B5FfBA48D199927816Eb7d26322FAD6F8F3D";
 
 //NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
 const transformCharacterData = (characterData) => {
